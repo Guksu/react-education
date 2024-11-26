0b391c086bea03ffada47e79494eb3e3bd66b85d
kimjongmin <yung3152@daum.net>
2024-11-26 12:18:00 +0900

    fix : Link 주석 수정 & 에러페이지 구성

--- pages/chapter7/index.tsx
+++ pages/chapter7/index.tsx
@@ -10,7 +10,7 @@ export default function Chapter7Router() {
       {/* <Link /> 컴포넌트의 prefetch를 사용하여 해당 페이지의 js를 먼저 로드할 수 있다.*/}
       {/* production환경에서만 적용되며, CSR은 해당되지 않는다.*/}
       {/* 단, 정적데이터만 가져오므로 getServerSideProps의 데이터는 가져오지 못한다. */}
-      {/* null / true / false 차이 :  https://nextjs-ko.org/docs/app/api-reference/components/link#prefetch*/}
+      {/*  true / false 차이 : https://nextjs.org/docs/pages/api-reference/components/link#prefetch*/}
       <Link href={"/chapter7/1"}>Second User Info</Link>
       <Link href={"/chapter8"}>Chapter 8</Link>
     </ul>
